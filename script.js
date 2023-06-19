ca64680c67d5fff1fe43d9b35e499c5bb9f27c77
LucianoQB18 <lhusoares@hotmail.com>
2023-06-19 02:29:55 -0300

    Final

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function toggleMode () {
   const html = document.documentElement;
   html.classList.toggle('light');
   const img = document.querySelector("#profile img");
-  if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/foto1.jpg")
-  }
+  //if (html.classList.contains("light")) {
+    //img.setAttribute("src", "./assets/foto1.jpg")
+  //}
   //else {
     //img.setAttribute("src", "./assets/foto1.jpg")
   //}
